fd0dc06cf336df251d7fcc977cc2adda1329a1f5
Olek <aleksander.marzec@udu.uekat.pl>
2025-12-16 08:14:28 +0100

    poprawki

--- zad2b.py
+++ zad2b.py
@@ -3,11 +3,16 @@ def multiply_by_two_for(liczby):
     for num in liczby:
         result.append(num * 2)
     return result
+
+
 lista = [1, 2, 3, 4, 5]
 print(multiply_by_two_for(lista))
 
+
 def pomnoz_przez_dwa(liczby):
     return [num * 2 for num in liczby]
+
+
 lista = [1, 2, 3, 4, 5]
 print(pomnoz_przez_dwa(lista))
 
